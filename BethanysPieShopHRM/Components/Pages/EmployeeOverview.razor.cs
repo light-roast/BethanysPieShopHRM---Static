17bd4279f67cf2cadb2044426fd7e43e3551ea63
Daniel Echeverri <daniel.echeverri@flyr.com>
2025-12-30 11:22:08 -0500

    Declare initialize life cycle method to EmployeeOverview Component class

--- BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -1,7 +1,14 @@
-﻿namespace BethanysPieShopHRM.Components.Pages
+﻿using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.Components.Pages
 {
 	public partial class EmployeeOverview
 	{
+		public List<Employee>? Employees { get; set; } = default!;
 
+		protected override void OnInitialized()
+		{
+			Employees = Services.MockDataService.Employees;
+		}
 	}
 }
